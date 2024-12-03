08fb82c405c66ad10b462a49b8bc500fabebfb0a
MichaelwDuda <mwd20001@gmail.com>
2024-12-03 09:06:59 -0500

    Added a data set

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List, Dict, Tuple, Callable
+import os
+import gc
+import traceback
+import warnings
+from pdb import set_trace
+
+import sklearn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
